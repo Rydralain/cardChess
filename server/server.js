882a6eb1b5f6b5f56070e080452c1607f4014180
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-11-11 21:48:24 -0700

    file resturcture, fix for static build

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 // Dependencies
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 var passport = require('passport');
 
 // Init App
@@ -12,21 +12,26 @@ const PORT = process.env.PORT || 4000
 // Require models for syncing
 const db = require('./models');
 
-// Serves built React code as static
-app.use(express.static("client/build"));
-
 //Body Parser Middleware 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
 
 // passport setup (PLS don't move this)
 require('./config/passport.js')(app, passport, db);
 
 // Import routes from controller folder so server has access to them
-//app.use(routes);
-
 require('./routes/auth.js')(app, passport);
 require('./routes/api.js')(app, db);
+
+//if (process.env.NODE_ENV === 'production') {
+
+    app.use(express.static(path.join(__dirname, '/../build/')));
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '/../build', 'index.html'));
+    });
+//}
 
 // Init server and begin listening
 db.sequelize.sync({}).then(function(){
